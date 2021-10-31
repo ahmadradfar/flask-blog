1823443375e200db1e38f5048efe3f1a5c1a9213
Ahmad Radfar <desktopmobile8@gmail.com>
2021-10-31 13:55:51 +0300

    Full code of the application added.

---
+++ blog/__init__.py
@@ -0,0 +1,40 @@
+import pdb
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+from blog.config import Config
+from flask_migrate import Migrate
+
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'user.login'
+login_manager.login_message_category = 'info'
+
+mail = Mail()
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    
+    with app.app_context():
+        db.init_app(app)
+        migrate.init_app(app, db)
+        bcrypt.init_app(app)
+        login_manager.init_app(app)
+        mail.init_app(app)    
+
+    from blog.user.routes import user
+    from blog.post.routes import post
+    from blog.main.routes import main
+    from blog.error.handler import error
+    app.register_blueprint(error)
+    app.register_blueprint(user)
+    app.register_blueprint(post)
+    app.register_blueprint(main)
+    
+
+    return app
---
+++ blog/user/forms.py
@@ -0,0 +1,69 @@
+from flask_wtf import FlaskForm
+from flask_login import current_user
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
+from blog.models import User
+
+
+class RegisterationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=30)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=255)])
+    confirm_password = PasswordField('Confrim Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+    cancel = SubmitField('Cancel')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username already exist. Please choose another one!')
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email already exist. Please choose another one!')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=255)])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+    cancel = SubmitField('Cancel')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=30)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    profile = FileField('Upload Profile Picture', validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
+    submit = SubmitField('Update')
+    cancel = SubmitField('Cancel')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Username already exist. Please choose another one!')
+    
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Email already exist. Please choose another one!')
+
+
+class ResetRequestForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('Email does not exist. Please first register!')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=255)])
+    confirm_password = PasswordField('Confrim Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password') 
